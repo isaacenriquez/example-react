8e3992d62576c7b2367faa37447475dc55f72826
Isaac Enriquez <isaac-e@live.com.mx>
2021-06-02 09:20:21 -0600

    current class

--- db_config.py
+++ db_config.py
@@ -3,9 +3,11 @@ from flask_pymongo import pymongo
 import os
 
 DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASWORD")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-client = pymongo.MongoClient("mongodb+srv://{DB_USER}:{DB_PASSWORD}@dbexample1.1x3hk.mongodb.net/{DB_NAME}}?retryWrites=true&w=majority")
+client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@dbexample1.1x3hk.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
 
-db = client.test
+# down here you need to add the collection name
+db = client.ClassExample
+
---
+++ main.py
@@ -0,0 +1,70 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource, reqparse, abort
+from flask_pymongo import pymongo
+from bson.json_util import dumps, ObjectId
+import db_config as database
+
+
+app=Flask(__name__)
+api=Api(app)
+
+""" 
+database.db.Badges.
+
+database is the config file
+db is the database name
+the next part is the collection
+
+"""
+
+class Test(Resource):
+    def get(self):
+        return jsonify({"message":"Test ok, connected"})
+
+
+class Badge(Resource):
+
+    def get(self, by, data):
+        response = self.abort_if_not_exist(by, data)
+        response['_id']=str(response['_id'])
+        return jsonify(response)
+
+    def post(self):
+        _id = str(database.db.Badges.insert_one(
+        {
+            'name':request.json['name'],
+            'last_name':request.json['last_name'],
+            'profile_picture':request.json['profile_picture'],
+            'hero_badge':request.json['hero_badge'],
+            'age':request.json['age'],
+            'city':request.json['city'],
+            'followers':request.json['followers'],
+            'likes':request.json['likes'],
+            'pictures':request.json['pictures'],
+        }
+        ).inserted_id)
+
+        return jsonify({"_id":_id})
+
+
+    def abort_if_not_exist(self, by, data):
+        if by== "_id":
+            response = database.db.Badges.find_one({"_id":ObjectId(data)})
+        else:
+            response = database.db.Badges.find_one({f"{by}":data})
+
+            if response:
+                return response
+            else:
+                abort(jsonify({"status":404,f"{by}":f"{data} not found"}))
+
+class AllBadges(Resource):
+    """ Gets all badges """
+    def get(self):
+        pass
+
+api.add_resource(Badge,'/new/', '/<string:by>:<string:data>')
+api.add_resource(Test,'/test/')
+
+if __name__ == '__main__':
+    app.run(load_dotenv=True)
