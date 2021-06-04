2cc138d2251904ff8a839c15d59a4cc2f422c3f9
Isaac Enriquez <isaac-e@live.com.mx>
2021-06-04 00:26:18 -0600

    adding badges

---
+++ res/badges.py
@@ -0,0 +1,19 @@
+from flask import jsonify, request
+from flask_restful import Resource, abort
+from flask_pymongo import pymongo
+from bson.json_util import ObjectId
+import db_config as database
+
+class Badges(Resource):
+    """ Gets all badges """
+    def get(self):
+        response = list(database.db.Badges.find())
+
+        for doc in response:
+            doc['_id'] = str(doc['_id'])
+        
+        return jsonify(response)
+
+    
+    def delete(self):
+        return database.db.Badges.delete_many({}).deleted_count
