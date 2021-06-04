f349e1dfdaf2996eafb56eccb30339e086b9a168
Isaac Enriquez <isaac-e@live.com.mx>
2021-06-04 01:41:12 -0600

    modify main

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ from res.badges import Badges
 app=Flask(__name__)
 api=Api(app)
 
+@app.route('/all/adults/')
+def get_adults():
+    response = list(database.db.Badges.find({'age':{"$gte: 21"}}))
+
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
+
+@app.route('/all/projection/')
+def get_name_and_age():
+    response = list(database.db.Badge.find({'age':{"$gte: 21"}},{'name':1,'age':1}))
+
+    for document in response:
+        document["_id"] = str(document['_id'])
+
+    return jsonify(response)
+
 
 
 api.add_resource(Badge,'/new/', '/<string:by>:<string:data>')
